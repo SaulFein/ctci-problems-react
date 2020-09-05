c8238443cc678980da0ccfef2a00368f4bd1cadd
Brandon <brandon.feinstein@hotmail.com>
2020-09-05 13:40:00 -0700

    solved prob

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import IsUnique from "./arrays-strings/is-unique";
 import TitleCase from "./code-wars/title-case";
+import UniqueIdApps from "./actual-interview-qs/apps-with-unique-authors-apple";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <TitleCase></TitleCase>
+      <UniqueIdApps></UniqueIdApps>
     </div>
   );
 }
--- src/actual-interview-qs/apps-with-unique-authors-apple.jsx
+++ src/actual-interview-qs/apps-with-unique-authors-apple.jsx
@@ -3,13 +3,37 @@ import React from "react";
 class UniqueIdApps extends React.Component {
   constructor() {
     super();
-    this.apps = {};
+    this.apps = [
+      { title: "Dumb App 1", authors: ["Jim Bob", "Carl Smith"] },
+      { title: "Dumb App 2", authors: ["Karen Smith", "Jim Bob"] },
+      { title: "Dumb App 3", authors: ["Kevin Smith", "Matt Goode"] },
+      { title: "Dumb App 4", authors: ["Karen Smith", "Bob Hope"] },
+      { title: "Dumb App 5", authors: ["Ralf Sutton", "Big Jay"] },
+    ];
   }
 
-  uniqueIdApp(apps) {}
+  uniqueIdApp(apps) {
+    const hashMap = {};
+    let idxsToRemove = [];
+    for (let i = 0; i < apps.length; i++) {
+      for (let j = 0; j < apps[i].authors.length; j++) {
+        if (hashMap[apps[i].authors[j]] >= 0) {
+          idxsToRemove.push(hashMap[apps[i].authors[j]]);
+          idxsToRemove.push(i);
+        } else {
+          hashMap[apps[i].authors[j]] = i;
+        }
+      }
+    }
+    let idxs = [...new Set(idxsToRemove)];
+    for (var i = idxs.length - 1; i >= 0; i--) {
+      apps.splice(idxs[i], 1);
+    }
+    return JSON.stringify(apps);
+  }
 
   render() {
-    return <div>{this.uniqueIdApp(["a", "b", "a"])}</div>;
+    return <div>{this.uniqueIdApp(this.apps)}</div>;
   }
 }
 
